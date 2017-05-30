52ed355a73eeac9b40fce96904fe787239c136d3
imroatul <imroatulhasanah9694@gmail.com>
2017-05-30 21:15:08 +0700

    template login

---
+++ application/views/Footer.php
@@ -0,0 +1,10 @@
+<footer>
+ 
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+</footer>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+  <script src="includes/js/index.js"></script>
+
+</body>
+</html>
--- application/views/login.php
+++ application/views/login.php
@@ -1,46 +1,28 @@
-<?php echo form_open("main/cek_login"); ?>
+<!---?php echo form_open("main/cek_login"); ?>
 Username
 <input type="text" name="namaAdmin">
 Password
 <input type="text" name="passwordAdmin">
 <button type="submit">Submit</button>
-<?php echo form_close(); ?>
-
-<!--DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="Dashboard">
-    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
-
-    <title>Tolah Toleh - Login Admin</title>
-
-    <link href="includes/css/bootstrap.css" rel="stylesheet">
-    <link href="includes/font-awesome/css/font-awesome.css" rel="stylesheet" />
-    <link href="includes/css/style.css" rel="stylesheet">
-    <link href="includes/css/style-responsive.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-	  <div id="login-page">
-	  	<div class="container">
-
-		      <form class="form-login" action="<!?php echo base_url()?>main/cek_login">
-		        <h2 class="form-login-heading">Halaman Login Admin</h2>
-		        <div class="login-wrap">
-		            <input type="text" class="form-control" name="namaAdmin" placeholder="masukkan nama admin" autofocus>
-		            <br>
-		            <input type="text" class="form-control" name="passwordAdmin" placeholder="masukkan password">
-                <br><br>
-		            <button class="btn btn-theme btn-block" name="submit "type="submit"><i class="fa fa-lock"></i> Login</button>
-            </div>
-          </form>
-
-	  	</div>
-	  </div>
-
-  </body>
-</html-->
+<!?php echo form_close(); ?-->
+<body>
+  <div class='preload login--container'>
+  <div class='login--form'>
+    <div class='login--username-container'>
+   <form action="<?php echo base_url();?>index.php/main/cek_login" method="post">
+      <label>Username</label>
+      <input autofocus placeholder='Username' type='text' name="namaAdmin">
+    </div>
+    <div class='login--password-container'>
+      <label>Password</label>
+      <input placeholder='Password' type='password' name="passwordAdmin">
+      <button class='js-toggle-login login--login-submit' type="submit">Login</button>
+    </div>
+</form>
+  </div>
+  <div class='login--toggle-container'>
+    <small>Selamat Datang,</small>
+    <div class='js-toggle-login'>Login</div>
+    <small>TolahToleh</small>
+  </div>
+</div>
